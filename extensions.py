312eb8f829c4dc7699e137750e9a1845a0a9b56f
KonstantinMaznev <knmaznev@icloud.com>
2023-04-19 00:43:13 +0300

    unsuccessful bug removal

--- extensions.py
+++ extensions.py
@@ -1,5 +1,6 @@
 import requests
 import json
+from config import keys
 
 class ConvertionException(Exception):
     pass
@@ -13,18 +14,18 @@ class Converter:
 
         try:
             quote_ticker = keys[quote]
-        except KeyError as e:
+        except KeyError:
             raise ConvertionException(f'Не удалось обработать валюту {quote}')
 
         try:
             base_ticker = keys[base]
-        except KeyError as e:
+        except KeyError:
             raise ConvertionException(f'He удалось обработать валюту {base}')
 
         try:
             amount = float(amount)
-        except ValueError as e:
+        except ValueError:
             raise ConvertionException(f'Не удалось обработать количество {amount}')
         r = requests.get(f"https://v6.exchangerate-api.com/v6/4951e59dc8f40f2a268ea857/pair/{base_ticker}/{quote_ticker}/{amount}")
-        total_base = json.loads(r.content)[quote_ticker]
+        total_base = json.loads(r.content)[keys[quote]]
         return total_base
--- values_bot.py
+++ values_bot.py
@@ -1,41 +1,10 @@
 import telebot
-import requests
-import json
-
-
-TOKEN = "5948700376:AAGZZm1zl5a3cgn2M5fHOPiyYwf4Tlo4cus"
+# import requests
+# import json
+from config import keys, TOKEN
+from extensions import ConvertionException, Converter
 
 bot = telebot.TeleBot(TOKEN)
-keys = dict(евро="EUR", доллар="USD", рубль="RUB")
-
-class ConvertionException(Exception):
-    pass
-
-class Converter:
-    @staticmethod
-    def get_price(base: str, quote: str, amount: str):
-
-        if quote == base:
-            raise ConvertionException(f" Невозможно перевести одинаковые валюты (base).")
-
-        try:
-            quote_ticker = keys[quote]
-        except KeyError:
-            raise ConvertionException(f'Не удалось обработать валюту {quote}')
-
-        try:
-            base_ticker = keys[base]
-        except KeyError:
-            raise ConvertionException(f'He удалось обработать валюту {base}')
-
-        try:
-            amount = float(amount)
-        except ValueError:
-            raise ConvertionException(f'Не удалось обработать количество {amount}')
-        r = requests.get(f"https://v6.exchangerate-api.com/v6/4951e59dc8f40f2a268ea857/pair/{base_ticker}/{quote_ticker}/{amount}")
-        total_base = json.loads(r.content)[keys[quote_ticker]]
-        return total_base
-
 
 
 @bot.message_handler(commands=['start', 'help'])
